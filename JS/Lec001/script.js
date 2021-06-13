e79f2d7a535e87f1093c3d2d03ac5c8e44bc3d7c
inventerlord <inventerlord@gmail.com>
2021-06-13 18:22:53 +0430

    JS-Lec001

---
+++ JS/Lec001/script.js
@@ -0,0 +1,19 @@
+// Datatypes in javascript
+let a = 10;
+let b = 20;
+let c = a+b;
+let age = 12; // number
+let myname = "Hassan";//String
+let person = {firstName:"Ramshan",secondName:"Abidi"};//object
+let isRaining = true; //boolean
+let grade;//random
+let nothing = null; //undefined
+
+
+console.log(age);
+console.log(myname);
+console.log(person.firstName+" "+person.secondName);
+console.log(isRaining);
+console.log(nothing);
+console.log(grade);
+document.write(c);
